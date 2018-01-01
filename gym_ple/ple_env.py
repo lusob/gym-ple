187f5462940fc304090a3184f99a4eeec1f1cbed
michaelrgb <michaelrgb@users.noreply.github.com>
2018-01-01 02:45:27 +0000

    Image from PLE is rotated, adjust observation space accordingly.

--- gym_ple/ple_env.py
+++ gym_ple/ple_env.py
@@ -20,7 +20,7 @@ class PLEEnv(gym.Env):
         self.game_state.init()
         self._action_set = self.game_state.getActionSet()
         self.action_space = spaces.Discrete(len(self._action_set))
-        self.screen_width, self.screen_height = self.game_state.getScreenDims()
+        self.screen_height, self.screen_width = self.game_state.getScreenDims()
         self.observation_space = spaces.Box(low=0, high=255, shape=(self.screen_width, self.screen_height, 3))
         self.viewer = None
 
